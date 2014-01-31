0085efec4dc5455dbb8e3315493e5348711d3a18
Nathan Neff <nathan.neff@gmail.com>
2014-01-30 20:25:37 -0500

    Actually *use*  param :-/

--- impala/parquet/run.sh
+++ impala/parquet/run.sh
@@ -4,6 +4,6 @@ if [[ -z "$IMPALAD" ]]; then
         exit 1
 fi
 echo "Running against Parquet table......"
-time impala-shell --impalad training01 -q "select ss_coupon_amt FROM tpcds.parquet_store_sales WHERE ss_coupon_amt IS NOT NULL ORDER BY ss_coupon_amt DESC LIMIT 10;"
+time impala-shell --impalad $IMPALAD -q "select ss_coupon_amt FROM tpcds.parquet_store_sales WHERE ss_coupon_amt IS NOT NULL ORDER BY ss_coupon_amt DESC LIMIT 10;"
 echo "Running against Text table......"
-time impala-shell --impalad training01 -q "select ss_coupon_amt FROM tpcds.store_sales WHERE ss_coupon_amt IS NOT NULL ORDER BY ss_coupon_amt DESC LIMIT 10;"
+time impala-shell --impalad $IMPALAD -q "select ss_coupon_amt FROM tpcds.store_sales WHERE ss_coupon_amt IS NOT NULL ORDER BY ss_coupon_amt DESC LIMIT 10;"
