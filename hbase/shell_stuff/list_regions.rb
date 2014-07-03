661fd25d5268b054fb3c78533da16125652e88d1
Nathan Neff <nathan.neff@gmail.com>
2014-07-03 13:10:41 -0400

    Region Locations returns Navigable Map
    Added unit tests and clearer printout of region names

--- hbase/shell_stuff/list_regions.rb
+++ hbase/shell_stuff/list_regions.rb
@@ -1,15 +1,21 @@
+require 'test/unit'
+extend Test::Unit::Assertions
+
 tbl = get_table('njn_transactions')
 # Get to the underlying tbl.table for the REAL power, my apprentice.
 puts "Here's the regions:"
+# get_region_locations returns a "NavigableMap" Java object that has a RegionInfo as a key, and ServerName as value
+# http://archive.cloudera.com/cdh5/cdh/5/hbase-0.96.1.1-cdh5.0.1/devapidocs/org/apache/hadoop/hbase/client/HTable.html#getRegionLocations%28%29
 tbl.table.get_region_locations.each_with_index do |region_thingy, idx|
         puts "-" * 100
         puts "Region " + idx.to_s
-        # Still haven't figured out what class region_thingy is but oh well.
-        # puts region_thingy[0].class
-        puts region_thingy[0].get_region_name_as_string
-        puts Bytes.toString(region_thingy[0].regionName)
+
+        # get_region_name_as_string is the same thing as .regionName(), except String vs. Byte Array
+        regionName = region_thingy[0].get_region_name_as_string
+        assert_equal regionName, Bytes.toString(region_thingy[0].regionName)
 
         # Print out info about this region
+        puts "Region Name is: " + regionName
         puts region_thingy[0].toString()
 end
 
