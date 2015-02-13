cb72027da5324e0ace33bfd578d93aa84adafca4
Nathan Neff <nathan.neff@gmail.com>
2015-02-13 16:28:45 -0500

    Two version table test

---
+++ hbase/shell_stuff/check_new_stuff/create_table_two_versions.rb
@@ -0,0 +1,12 @@
+# Create tbl w/two versions
+tbl = 'test_two_vers'
+create tbl, {NAME => 'desc', VERSIONS => 2}
+describe tbl
+
+put tbl, 'Empire', 'desc:title', 'Empire Wimps Out'
+put tbl, 'Empire', 'desc:title', 'Empire Strikes Back'
+
+get tbl, 'Empire', { COLUMN=>'desc:title', VERSIONS=> 2}
+
+disable tbl
+drop tbl
