733a17082c803b68bd8b62f4bef1cb7910cc4c46
Nathan Neff <nathan.neff@gmail.com>
2015-05-06 22:40:01 -0500

    Add AVG example with windowing/analytic
    Also, output data to different output files

--- impala/analytic-functions/run.sh
+++ impala/analytic-functions/run.sh
@@ -1,4 +1,6 @@
 hdfs dfs -mkdir -p /dualcore/ads
 hdfs dfs -put ads.txt /dualcore/ads
 impala-shell -f create_ads.sql
-impala-shell -f analyze_ads.sql
+impala-shell -f lag_ads.sql -o lag_ads.txt
+impala-shell -f avg_ads.sql -o avg_ads.txt
+echo "Look in lag_ads.txt, and avg_ads.txt"
