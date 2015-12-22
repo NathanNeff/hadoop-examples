a4e009f613361447464da5c2806c2e765bb956dc
Nathan Neff <nathan.neff@gmail.com>
2015-12-22 13:28:15 -0600

    Reformat / rename variables and correct .getName method

--- hbase/apitests/src/main/java/apitests/CreateTable.java
+++ hbase/apitests/src/main/java/apitests/CreateTable.java
@@ -1,21 +1,20 @@
 package apitests;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.conf.Configuration;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HConnection;
 import org.apache.hadoop.hbase.client.HConnectionManager;
-import org.apache.hadoop.hbase.TableName;
-
+import org.apache.hadoop.hbase.util.Bytes;
 
 public class CreateTable {
 
-	public static final String TEST_TABLE = "test_movie";
-	public static final byte[] TEST_TABLE_BYTES = TableName.getName(TEST_TABLE);
-	public static final String DESC_COLFAM = "desc";
-	public static final byte[] DESC_COLFAM_BYTES = DESC_COLFAM.getBytes();
+	public static final TableName TEST_TABLE = TableName
+			.valueOf("test_create_table");
+	public static final byte[] CF_INFO = Bytes.toBytes("info");
 
 	/**
 	 * Creates the table in HBase based on the row key type and splits
@@ -23,32 +22,30 @@ public class CreateTable {
 	 * @throws Exception
 	 */
 	public void createTable(int connectionMethod) throws Exception {
-                HBaseAdmin admin;
-                if (connectionMethod == 0) {
-                        // This is the way that doesn't work (I don't know why yet)
-                        admin = new HBaseAdmin(new Configuration());
-                }
-                else if (connectionMethod == 1) {
-                        // This works on CDH 5.3.1
-                        admin = new HBaseAdmin(HBaseConfiguration.create());
-                }
-                else {
-                        // This works on CDH 5.3.1
-                        HConnection connection = 
-                                HConnectionManager.createConnection(HBaseConfiguration.create());
-                        admin = new HBaseAdmin(connection);
-                }
+		HBaseAdmin admin;
+		if (connectionMethod == 0) {
+			// This is the way that doesn't work (I don't know why yet)
+			admin = new HBaseAdmin(new Configuration());
+		} else if (connectionMethod == 1) {
+			// This works on CDH 5.3.1
+			admin = new HBaseAdmin(HBaseConfiguration.create());
+		} else {
+			// This works on CDH 5.3.1
+			HConnection connection = HConnectionManager
+					.createConnection(HBaseConfiguration.create());
+			admin = new HBaseAdmin(connection);
+		}
 
-		if (admin.tableExists(TEST_TABLE_BYTES)) {
-                        admin.disableTable(TEST_TABLE_BYTES);
-                        admin.deleteTable(TEST_TABLE_BYTES);
+		if (admin.tableExists(TEST_TABLE)) {
+			admin.disableTable(TEST_TABLE);
+			admin.deleteTable(TEST_TABLE);
 		}
 
 		// Create the new table
 		HTableDescriptor tableDescriptor = new HTableDescriptor();
-		tableDescriptor.setName(TEST_TABLE_BYTES);
+		tableDescriptor.setName(TEST_TABLE);
 
-		HColumnDescriptor columnDescriptor = new HColumnDescriptor(DESC_COLFAM_BYTES);
+		HColumnDescriptor columnDescriptor = new HColumnDescriptor(CF_INFO);
 		tableDescriptor.addFamily(columnDescriptor);
 
 		admin.createTable(tableDescriptor);
@@ -56,13 +53,13 @@ public class CreateTable {
 	}
 
 	public static void main(String[] args) {
-                int connectionMethod = 2;
+		int connectionMethod = 2;
 
-                try {
-                        CreateTable ct = new CreateTable();
-                        ct.createTable(connectionMethod);
-                } catch (Exception e) {
-                        e.printStackTrace();
-                }
-        }
+		try {
+			CreateTable ct = new CreateTable();
+			ct.createTable(connectionMethod);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
