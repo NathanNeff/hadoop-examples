c850744344792ac73aa01b448877f4129b329da0
Nathan Neff <nathan.neff@gmail.com>
2017-10-01 13:26:07 -0500

    Specify Schema for CSV import

--- spark/dataframes/schemasCSV.scala
+++ spark/dataframes/schemasCSV.scala
@@ -4,4 +4,17 @@ val opts = Map("header" -> "true",
                "inferSchema" -> "true")
 val csvDF = spark.read.options(opts).csv("csvdata")
 csvDF.printSchema()
-csvDF.show()
+// csvDF.show()
+
+
+// Try specifying a schema and see what happens
+import org.apache.spark.sql.types.{StringType,StructField,StructType}
+val columnsList = List(
+	StructField("firstname", StringType),
+	StructField("lastname", StringType)
+)
+
+val peopleSchema = StructType(columnsList)
+val csvDFWithSchema = spark.read.option("header", "true").
+    schema(peopleSchema).csv("csvdata")
+csvDFWithSchema.show()
