e012ad9feae4f77bf1b592da4d2a4da5513eb8e7
Nathan Neff <nathan.neff@gmail.com>
2017-10-01 16:34:40 -0500

    Right outer and WHERE join examples

--- spark/dataframes/joins.scala
+++ spark/dataframes/joins.scala
@@ -2,5 +2,11 @@
 val dfPeople = spark.read.json("people.json")
 val dfInterests = spark.read.json("interests.json")
 
-val dfPeopleAndInterests = dfPeople.join(dfInterests, lower($"firstName") === lower($"fname"), "left_outer")
-dfPeopleAndInterests.select("firstName", "interest").show()
+dfPeople.join(dfInterests, lower($"firstName") === lower($"fname"), "left_outer").
+		select("firstName", "interest").show()
+
+dfPeople.join(dfInterests, lower($"firstName") === lower($"fname"), "right_outer").
+		select("firstName", "interest").show()
+
+dfPeople.join(dfInterests).where($"firstName" === $"fname").
+		select("firstName", "interest").show()
