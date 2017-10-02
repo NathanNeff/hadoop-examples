a906dd25e6f017dd95ea45c59c4016a6aea3758a
Nathan Neff <nathan.neff@gmail.com>
2017-10-01 19:12:16 -0500

    Example of GROUP BY, counting NULL agg.

--- spark/dataframes/grouping.scala
+++ spark/dataframes/grouping.scala
@@ -7,3 +7,24 @@ val grpd = df.groupBy("testScore").agg(min($"firstName") as "First FirstName",
                                        sum($"studyTime") as "all_studytime").
                                    where($"all_studytime" > 100)
 grpd.show()
+
+// Grouping with LEFT OUTER JOIN, and showing zero for
+// count of rows with no corresponding child elements
+
+val customerSeq = Seq("""{ "firstName":"Nate",   "id":1 }""",
+                      """{ "firstName":"Jackie", "id":2 }""")
+val customersDS = spark.createDataset(customerSeq)
+val customersDF = spark.read.json(customersDS)
+
+val ordersSeq = Seq("""{ "cust_id":1, "product":"Something", "order_id":2}""")
+val ordersDS = spark.createDataset(ordersSeq)
+val ordersDF = spark.read.json(ordersDS)
+
+val customerOrders = customersDF.join(ordersDF, $"id" === $"cust_id", "left_outer")
+
+val customerOrderCounts = customerOrders.
+    groupBy($"firstName", $"id").
+    agg(count($"cust_id") as "num_orders")
+
+customerOrderCounts.show()
+
