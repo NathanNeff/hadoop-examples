44c4d4daaaa345ee8393af411f58044f12717b54
Nathan Neff <nathan.neff@gmail.com>
2017-10-05 20:25:07 -0500

    "dynamic" way of adding cols.

--- spark/dataframes/withColumn.scala
+++ spark/dataframes/withColumn.scala
@@ -1,8 +1,12 @@
 val flightsSeq = Seq("""{ "flight_num":1,   "dt":"2017-01-01" }""",
-                      """{ "flight_num":2, "dt":"2017-01-02" }""")
+                      """{ "flight_num":2, "dt":"2017-01-02" }""",
+                      """{ "flight_num":3, "dt":"2017-01-03" }""",
+                      """{ "flight_num":4, "dt":"2017-01-04" }""")
+
 val stringRDD = sc.parallelize(flightsSeq)
 val flightsDS = spark.read.json(stringRDD)
 
+// There's a better way to do this (see below!)
 val enhanced = flightsDS.withColumn(
     "DayOfWeek", date_format($"dt", "E")).withColumn(
     "isSaturday", date_format($"dt", "E") === "Sat").withColumn(
@@ -13,4 +17,14 @@ val enhanced = flightsDS.withColumn(
     "isThursday", date_format($"dt", "E") === "Thu").withColumn(
     "isFriday", date_format($"dt", "E") === "Fri")
 
+
 enhanced.show()
+
+// There's a better (e.g. functional way to do this but for now . . . .)
+var enhancedBetter = flightsDS
+for (day <- Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat")) {
+    enhancedBetter = 
+        enhancedBetter.withColumn("is" + day,  
+                            date_format($"dt", "E") === day)
+}
+               
