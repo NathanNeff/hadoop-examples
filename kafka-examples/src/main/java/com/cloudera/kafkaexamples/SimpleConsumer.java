4fed34205dd4e63f2355dd9bf2fbfdfe9d1c211c
Nathan Neff <nathan.neff@gmail.com>
2018-10-07 18:22:49 -0500

    Add Kafka Examples from Cloudera

---
+++ kafka-examples/src/main/java/com/cloudera/kafkaexamples/SimpleConsumer.java
@@ -0,0 +1,48 @@
+package com.cloudera.kafkaexamples;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+public class SimpleConsumer {
+  public static void main(String[] args) {
+
+    // Set up client Java properties
+    Properties props = new Properties();
+    props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+        "192.168.201.149:9092");
+    // Just a user-defined string to identify the consumer group
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "test_consumer_group");
+    // Enable auto offset commit
+    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+    props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+    props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+        StringDeserializer.class.getName());
+    props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+        StringDeserializer.class.getName());
+
+    KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+
+          // List of topics to subscribe to
+    consumer.subscribe(Arrays.asList("ufo_sightings"));
+    while (true) {
+      try {
+        ConsumerRecords<String, String> records = consumer.poll(100);
+        for (ConsumerRecord<String, String> record : records) {
+          System.out.printf("Partition = %d\n", record.partition());
+          System.out.printf("Offset = %d\n", record.offset());
+          System.out.printf("Key    = %s\n", record.key());
+          System.out.printf("Value  = %s\n", record.value());
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      } 
+    }
+  }
+}
+        
---
+++ kafka-examples/src/main/java/com/cloudera/kafkaexamples/SimpleProducer.java
@@ -0,0 +1,43 @@
+package com.cloudera.kafkaexamples;
+
+import java.util.Date;
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class SimpleProducer {
+	public static void main(String[] args) {
+		// Generate total consecutive events starting with ufoId
+		long total = 100000L;
+		long ufoId = 0L;
+
+		// Set up client Java properties
+		Properties props = new Properties();
+		props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "192.168.201.149:9092");
+
+		props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		props.setProperty(ProducerConfig.ACKS_CONFIG, "1");
+
+		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+
+		for (long i = 0; i < total; i++) {
+			String key = Long.toString(ufoId++);
+			long runtime = new Date().getTime();
+			double latitude = (Math.random() * (2 * 85.05112878)) - 85.05112878;
+			double longitude = (Math.random() * 360.0) - 180.0;
+			String msg = key + "," + latitude + "," + longitude;
+			try {
+				ProducerRecord<String, String> data = new ProducerRecord<String, String>("ufo_sightings", key, msg);
+				producer.send(data);
+				// long wait = Math.round(Math.random() * 25);
+				// Thread.sleep(wait);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
