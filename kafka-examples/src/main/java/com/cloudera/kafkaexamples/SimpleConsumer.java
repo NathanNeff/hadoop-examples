797029ed6f76ef8532a56dd2d32c996eef848ed3
Nathan Neff <nathan.neff@gmail.com>
2018-10-16 12:15:38 -0500

    Indent code using spaces v. tabs

--- kafka-examples/src/main/java/com/cloudera/kafkaexamples/SimpleConsumer.java
+++ kafka-examples/src/main/java/com/cloudera/kafkaexamples/SimpleConsumer.java
@@ -13,85 +13,84 @@ import java.util.UUID;
 
 
 public class SimpleConsumer {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    // create Options object
-    Options options = new Options();
+        // create Options object
+        Options options = new Options();
 
-    Option fromBeginningOption = Option.builder()
-            .longOpt( "from-beginning" )
-            .desc("Read topic from beginning")
-            .hasArg(false)
-            .argName("from-beginning")
-            .build();
+        Option fromBeginningOption = Option.builder()
+                .longOpt("from-beginning")
+                .desc("Read topic from beginning")
+                .hasArg(false)
+                .argName("from-beginning")
+                .build();
 
-    options.addOption(fromBeginningOption);
+        options.addOption(fromBeginningOption);
 
-    Option groupOption = Option.builder()
-            .longOpt("group-id")
-            .desc("Consumer Group to Join")
-            .hasArg(true)
-            .argName("Consumer Group to Join (default is a UUID)")
-            .build();
+        Option groupOption = Option.builder()
+                .longOpt("group-id")
+                .desc("Consumer Group to Join")
+                .hasArg(true)
+                .argName("Consumer Group to Join (default is a UUID)")
+                .build();
 
-    options.addOption(groupOption);
+        options.addOption(groupOption);
 
-    CommandLineParser parser = new DefaultParser();
-    CommandLine cmd = null;
-    try {
-      cmd = parser.parse( options, args);
-    } catch (ParseException e) {
-      System.err.println( "Parsing failed.  Reason: " + e.getMessage() );
-      System.exit(1);
-    }
-
-    // Set up client Java properties
-    Properties props = new Properties();
-    props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-        "quickstart.cloudera:9092");
-    // Just a user-defined string to identify the consumer group
-    // Enable auto offset commit
-    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
-    props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
-    props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-        StringDeserializer.class.getName());
-    props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-        StringDeserializer.class.getName());
-
-
-    if (null != cmd && cmd.hasOption("group-id")) {
-      props.put(ConsumerConfig.GROUP_ID_CONFIG, cmd.getOptionValue("group-id"));
-    }
-    else {
-      props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
-    }
-
-    if (null != cmd && cmd.hasOption("from-beginning")) {
-      // This is confusing versus using seekToBeginning(partitions).  The problem is we have not
-      // been assigned any partitions yet, so seekToBeginning doesn't work without
-      // calling poll(0)
-      // See https://grokbase.com/t/kafka/users/16384874pk/seektobeginning-doesnt-work-without-auto-offset-reset
-      props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-      System.out.println("Setting to earliest");
-    }
-
-    // List of topics to subscribe to
-    try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
-      // List of topics to subscribe to
-      consumer.subscribe(Arrays.asList("ufo_sightings"));
-      while (true) {
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
         try {
-          ConsumerRecords<String, String> records = consumer.poll(100);
-          for (ConsumerRecord<String, String> record : records) {
-            System.out.printf("Offset = %d\n", record.offset());
-            System.out.printf("Key    = %s\n", record.key());
-            System.out.printf("Value  = %s\n", record.value());
-          }
-        } catch (Exception e) {
-          e.printStackTrace();
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.err.println("Parsing failed.  Reason: " + e.getMessage());
+            System.exit(1);
         }
-      }
+
+        // Set up client Java properties
+        Properties props = new Properties();
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "quickstart.cloudera:9092");
+        // Just a user-defined string to identify the consumer group
+        // Enable auto offset commit
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                StringDeserializer.class.getName());
+
+
+        if (null != cmd && cmd.hasOption("group-id")) {
+            props.put(ConsumerConfig.GROUP_ID_CONFIG, cmd.getOptionValue("group-id"));
+        } else {
+            props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
+        }
+
+        if (null != cmd && cmd.hasOption("from-beginning")) {
+            // This is confusing versus using seekToBeginning(partitions).  The problem is we have not
+            // been assigned any partitions yet, so seekToBeginning doesn't work without
+            // calling poll(0)
+            // See https://grokbase.com/t/kafka/users/16384874pk/seektobeginning-doesnt-work-without-auto-offset-reset
+            props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+            System.out.println("Setting to earliest");
+        }
+
+        // List of topics to subscribe to
+        try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
+            // List of topics to subscribe to
+            consumer.subscribe(Arrays.asList("ufo_sightings"));
+            while (true) {
+                try {
+                    ConsumerRecords<String, String> records = consumer.poll(100);
+                    for (ConsumerRecord<String, String> record : records) {
+                        System.out.printf("Offset = %d\n", record.offset());
+                        System.out.printf("Key    = %s\n", record.key());
+                        System.out.printf("Value  = %s\n", record.value());
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
-  }
 }
         
