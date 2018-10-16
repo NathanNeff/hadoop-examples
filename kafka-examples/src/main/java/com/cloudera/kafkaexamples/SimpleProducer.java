7701cc6fd83216cce207ea5b74a3a61dc772fbb2
Nathan Neff <nathan.neff@gmail.com>
2018-10-16 12:13:19 -0500

    Use try-with-resources for Producer

--- kafka-examples/src/main/java/com/cloudera/kafkaexamples/SimpleProducer.java
+++ kafka-examples/src/main/java/com/cloudera/kafkaexamples/SimpleProducer.java
@@ -22,21 +22,22 @@ public class SimpleProducer {
 		props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 		props.setProperty(ProducerConfig.ACKS_CONFIG, "1");
 
-		KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
+		try (KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props)) {
 
-		for (long i = 0; i < total; i++) {
-			String key = Long.toString(ufoId++);
-			long runtime = new Date().getTime();
-			double latitude = (Math.random() * (2 * 85.05112878)) - 85.05112878;
-			double longitude = (Math.random() * 360.0) - 180.0;
-			String msg = key + "," + latitude + "," + longitude;
-			try {
-				ProducerRecord<String, String> data = new ProducerRecord<String, String>("ufo_sightings", key, msg);
-				producer.send(data);
-				// long wait = Math.round(Math.random() * 25);
-				// Thread.sleep(wait);
-			} catch (Exception e) {
-				e.printStackTrace();
+			for (long i = 0; i < total; i++) {
+				String key = Long.toString(ufoId++);
+				long runtime = new Date().getTime();
+				double latitude = (Math.random() * (2 * 85.05112878)) - 85.05112878;
+				double longitude = (Math.random() * 360.0) - 180.0;
+				String msg = key + "," + latitude + "," + longitude;
+				try {
+					ProducerRecord<String, String> data = new ProducerRecord<String, String>("ufo_sightings", key, msg);
+					producer.send(data);
+					// long wait = Math.round(Math.random() * 25);
+					// Thread.sleep(wait);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
