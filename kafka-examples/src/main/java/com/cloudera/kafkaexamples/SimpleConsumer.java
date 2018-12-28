8cf62b7f12867bf0ffb085a5b18f4e0b736f738a
Nathan Neff <nathan.neff@gmail.com>
2018-12-27 18:52:49 -0600

    Complete revamp of SimpleConsumer
    No new functionality, with the exception
    of printHelp call in the case of invalid
    arguments.
    
    Instantiate SimpleConsumer and attempt
    to use object-oriented patterns.

--- kafka-examples/src/main/java/com/cloudera/kafkaexamples/SimpleConsumer.java
+++ kafka-examples/src/main/java/com/cloudera/kafkaexamples/SimpleConsumer.java
@@ -1,5 +1,6 @@
 package com.cloudera.kafkaexamples;
 
+import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Properties;
 import java.util.UUID;
@@ -7,6 +8,7 @@ import java.util.UUID;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
@@ -16,103 +18,96 @@ import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-
 public class SimpleConsumer {
-    public static void main(String[] args) {
+    Options options = new Options();
+    CommandLine cmd = null;
+    Properties props = new Properties();
 
-        // create Options object
-        Options options = new Options();
+    private static final String BOOTSTRAP_SERVERS_CONFIG = "bootstrapservers";
+    private static final String FROM_BEGINNING_CONFIG = "frombeginning";
+    private static final String GROUP_ID_CONFIG = "groupid";
+    private static final String TOPIC_CONFIG = "topic";
 
-        Option topicOption = Option.builder("topic")
-                .longOpt("topic")
-                .required(true)
-                .desc("Topic to subscribe to")
-                .hasArg(true)
-                .argName("topic").build();
-        options.addOption(topicOption);
+    private void configure() {
 
-        Option bootstrapOption = Option.builder("bootstrapserver")
-                .longOpt("bootstrap-server")
-                .desc("Kafka brokers to use in bootstrap")
-                .required(true)
-                .hasArg(true)
-                .argName("bootstrap-server").build();
-        options.addOption(bootstrapOption);
-
-        Option fromBeginningOption = Option.builder("frombeginning")
-                .longOpt("from-beginning")
-                .desc("Read topic from beginning")
-                .hasArg(false)
-                .argName("from-beginning")
-                .build();
-
-        options.addOption(fromBeginningOption);
-
-        Option groupOption = Option.builder("groupid")
-                .longOpt("group-id")
-                .desc("Consumer Group to Join")
-                .hasArg(true)
-                .argName("Consumer Group to Join (default is a UUID)")
-                .build();
-
-        options.addOption(groupOption);
-
-        CommandLineParser parser = new DefaultParser();
-        CommandLine cmd = null;
-        try {
-            cmd = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.err.println("Parsing failed.  Reason: " + e.getMessage());
-            System.exit(1);
-        }
-
-        // Set up client Java properties
-        Properties props = new Properties();
-        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
-                cmd.getOptionValue(bootstrapOption.getOpt()));
-        // Just a user-defined string to identify the consumer group
-        // Enable auto offset commit
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
-        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class.getName());
-        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class.getName());
-
-
-        if (null != cmd && cmd.hasOption(groupOption.getOpt())) {
-            props.put(ConsumerConfig.GROUP_ID_CONFIG, cmd.getOptionValue(groupOption.getOpt()));
+        // Set up client properties according to commandline
+        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, cmd.getOptionValue(BOOTSTRAP_SERVERS_CONFIG));
+        if (null != cmd && cmd.hasOption(GROUP_ID_CONFIG)) {
+            props.put(ConsumerConfig.GROUP_ID_CONFIG, cmd.getOptionValue(GROUP_ID_CONFIG));
         } else {
             props.put(ConsumerConfig.GROUP_ID_CONFIG, UUID.randomUUID().toString());
         }
 
-        // List of topics to subscribe to
+        // Set up hard-coded properties :(
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+    }
+
+    public void parseArgs(String[] args) throws ParseException {
+
+        Option topicOption = Option.builder(TOPIC_CONFIG)
+                .longOpt("topic")
+                .required(true)
+                .desc("Topic to subscribe to")
+                .hasArg(true).argName("topic").build();
+
+        options.addOption(topicOption);
+
+        Option bootstrapOption = Option.builder(BOOTSTRAP_SERVERS_CONFIG)
+                .longOpt("bootstrap-server")
+                .desc("Kafka brokers to use in bootstrap")
+                .required(true)
+                .hasArg(true)
+                .argName("bootstrap-server")
+                .build();
+        options.addOption(bootstrapOption);
+
+        Option fromBeginningOption = Option.builder(FROM_BEGINNING_CONFIG)
+                .longOpt("from-beginning")
+                .desc("Read topic from beginning")
+                .hasArg(false)
+                .argName("from-beginning").build();
+
+        options.addOption(fromBeginningOption);
+
+        Option groupOption = Option.builder(GROUP_ID_CONFIG)
+                .longOpt("group-id")
+                .desc("Consumer Group to Join")
+                .hasArg(true)
+                .argName("Consumer Group to Join (default is a UUID)").build();
+
+        options.addOption(groupOption);
+        CommandLineParser parser = new DefaultParser();
+        cmd = parser.parse(options, args);
+    }
+
+    public void startConsuming() {
         try (KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props)) {
-            if (null != cmd && cmd.hasOption(fromBeginningOption.getOpt())) {
-                /* 
+            if (null != cmd && cmd.hasOption(FROM_BEGINNING_CONFIG)) {
+                /*
                  * This program does two things when "from-beginning" is specified:
                  * 
-                 * 1) What to do if there are no tracked offsets for a particular group
-                 *    or partition.  "earliest", "latest", and none which will throw an 
-                 *    exception.  Setting "--from-beginning" tells this program to opt for
-                 *    "earliest" offset of a topic/partition if there is no tracked offset
-                 *    for this group.
-                 *    
-                 * 2) Seek To Beginning - We use an event listener which seeksToBeginning
-                 *    upon topic/partition assignments.
-                 * This is confusing versus using seekToBeginning(partitions).  The problem is we have not
-                 * been assigned any partitions yet, so seekToBeginning doesn't work without
-                 * calling poll(0)
-                 * See https://grokbase.com/t/kafka/users/16384874pk/seektobeginning-doesnt-work-without-auto-offset-reset
+                 * 1) What to do if there are no tracked offsets for a particular group or
+                 * partition. "earliest", "latest", and none which will throw an exception.
+                 * Setting "--from-beginning" tells this program to opt for "earliest" offset of
+                 * a topic/partition if there is no tracked offset for this group.
+                 * 
+                 * 2) Seek To Beginning - We use an event listener which seeksToBeginning upon
+                 * topic/partition assignments. This is confusing versus using
+                 * seekToBeginning(partitions). The problem is we have not been assigned any
+                 * partitions yet, so seekToBeginning doesn't work without calling poll(0) See
+                 * https://grokbase.com/t/kafka/users/16384874pk/seektobeginning-doesnt-work-
+                 * without-auto-offset-reset
                  */
                 props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
                 System.out.println("Will seek to beginning, when topics/partitions are assigned.");
-                consumer.subscribe(Arrays.asList(cmd.getOptionValue(topicOption.getOpt())),
+                consumer.subscribe(Arrays.asList(cmd.getOptionValue(TOPIC_CONFIG)),
                         new SeekToBeginningListener(consumer));
-            }
-            else {
-                consumer.subscribe(Arrays.asList(cmd.getOptionValue(topicOption.getOpt())));
+            } else {
+                consumer.subscribe(Arrays.asList(cmd.getOptionValue(TOPIC_CONFIG)));
             }
 
             while (true) {
@@ -129,5 +124,21 @@ public class SimpleConsumer {
             }
         }
     }
+
+    public static void main(String[] args) {
+
+        SimpleConsumer sc = new SimpleConsumer();
+        try {
+            sc.parseArgs(args);
+        } catch (ParseException pe) {
+            System.err.println(pe.getMessage());
+            HelpFormatter formatter = new HelpFormatter();
+            PrintWriter pw = new PrintWriter(System.err);
+            formatter.printHelp(pw, 120, "SimpleConsumer", "", sc.options, 5, 10, "");
+            pw.close();
+            System.exit(1);
+        }
+        sc.configure();
+        sc.startConsuming();
+    }
 }
-        
