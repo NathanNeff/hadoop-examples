34abf466f3047ff2f83b82ba5f891f9f1f3dc4f6
Nathan Neff <nathan.neff@gmail.com>
2020-02-16 13:32:45 -0600

    Add warning about UDFs

--- spark/structured_streaming_sensors/rate_source_with_more_data.py
+++ spark/structured_streaming_sensors/rate_source_with_more_data.py
@@ -1,10 +1,24 @@
 """
 Simple example of Spark Structured Streaming's rate source
+This code uses a UDF to generate fake sensor data
+*NOTE* Be sure to read Jacek Laskowski's recommendations on UDFs:
+https://jaceklaskowski.gitbooks.io/mastering-spark-sql/spark-sql-udfs-blackbox.html
 """
 import random
 from pyspark.sql           import SparkSession
-from pyspark.sql.types     import IntegerType
+from pyspark.sql.types     import StructType, StructField, IntegerType, StringType
 from pyspark.sql.functions import udf
+
+# Define a StructType to be able to return complex / multiple
+# fields from a UDF call.
+# Thanks to https://stackoverflow.com/questions/35322764/
+# and Cloudera Developer Course:
+# https://www.cloudera.com/about/training/courses/developer-training-for-spark-and-hadoop.html
+SENSOR_RECORD_TYPE = StructType([
+    StructField("machine", StringType()),
+    StructField("sensor_id", IntegerType()),
+    StructField("sensor_value", IntegerType()),
+    StructField("err_code", StringType())])
 
 NUM_MACHINES = 100
 
