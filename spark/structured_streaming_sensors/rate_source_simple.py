d4ae083965877c8a2949d10fd418536311de4450
Nathan Neff <nathan.neff@gmail.com>
2020-02-16 18:59:28 -0600

    Add another example of renaming a column

--- spark/structured_streaming_sensors/rate_source_simple.py
+++ spark/structured_streaming_sensors/rate_source_simple.py
@@ -1,5 +1,8 @@
 """
 Simple example of Spark Structured Streaming's rate source
+
+References:  Changing a column's name:
+  - https://stackoverflow.com/questions/34077353
 """
 from pyspark.sql import SparkSession
 
@@ -15,4 +18,10 @@ rateDF = spark.readStream.format("rate").option("rowsPerSecond", 50).load()
 
 rateQuery = rateDF.writeStream.format("console").option("truncate", False).start()
 
+# Remove all columns except "timestamp" and rename
+renamedQuery = rateDF.selectExpr("timestamp AS ts").\
+    writeStream.\
+    format("console").option("truncate", False).start()
+
 # Call rateQuery.stop()
+# Call renamedQuery.stop()
